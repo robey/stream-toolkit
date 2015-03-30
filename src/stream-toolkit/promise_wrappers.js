ea862987975f0a52c15ea1cc3f3a83589de81bdb
Robey Pointer <robeypointer@gmail.com>
2015-03-29 20:23:21 -0700

    make these more promisy.

--- src/stream-toolkit/promise_wrappers.js
+++ src/stream-toolkit/promise_wrappers.js
@@ -23,19 +23,37 @@ function handleOneShots(obj, handlers) {
 }
 
 /*
- * add a one-shot handler to an event.
- * the handler is removed once the event fires OR a promise is resolved.
+ * add one-shot event handlers, attached to a new promise.
+ * the handler is removed once the event fires OR the promise is complete.
  * this is useful for attaching multiple events to the same promise.
  * for example, an object may emit a "data" event on success and an "error"
- * event on failure, and you'd like to merge them into one promise.
+ * event on failure, and you'd like to merge them into one promise, and remove
+ * the event handlers on the way out.
+ *
+ * f: (resolve, reject, onEvent) => <code>
+ * onEvent(obj, eventName, handler)
  */
-function untilPromise(promise, obj, eventName, handler) {
-  function h(...x) {
-    obj.removeListener(eventName, h);
-    return handler(...x);
+function untilPromise(f) {
+  // some gymnastics here, so we can use the new "new Promise((resolve, reject))" API.
+  const handlers = [];
+  function onEvent(obj, eventName, handler) {
+    handlers.push({ obj, eventName, handler });
   }
-  obj.on(eventName, h);
-  promise.finally(() => obj.removeListener(eventName, h));
+
+  const promise = new Promise((resolve, reject) => {
+    f(resolve, reject, onEvent);
+  });
+
+  handlers.forEach(({ obj, eventName, handler }) => {
+    function h(...x) {
+      obj.removeListener(eventName, h);
+      return handler(...x);
+    }
+    obj.on(eventName, h);
+    promise.finally(() => obj.removeListener(eventName, h));
+  });
+
+  return promise;
 }
 
 // add promise-based methods to a stream
@@ -46,19 +64,19 @@ function promisify(stream) {
   stream.endPromise = () => {
     // if the stream is already closed, we won't get another "end" event, so check the stream's state.
     if (stream._readableState && stream._readableState.endEmitted) return Promise.resolve();
-    const deferred = Promise.defer();
-    untilPromise(deferred.promise, stream, "error", (error) => deferred.reject(error));
-    untilPromise(deferred.promise, stream, "end", () => deferred.resolve());
-    return deferred.promise;
+    return untilPromise((resolve, reject, onEvent) => {
+      onEvent(stream, "error", reject);
+      onEvent(stream, "end", resolve);
+    });
   };
 
   stream.finishPromise = () => {
     // if the stream is already closed, we won't get another "finish" event, so check the stream's state.
     if (stream._writableState && stream._writableState.finished) return Promise.resolve();
-    const deferred = Promise.defer();
-    untilPromise(deferred.promise, stream, "error", (error) => deferred.reject(error));
-    untilPromise(deferred.promise, stream, "finish", () => deferred.resolve());
-    return deferred.promise;
+    return untilPromise((resolve, reject, onEvent) => {
+      onEvent(stream, "error", reject);
+      onEvent(stream, "finish", resolve);
+    });
   };
 
   // turn a stream.read(N) into a function that returns a promise.
